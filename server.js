4a88f9ff282b0454b418d7bf65a9c5aca0ab07eb
Ayushkumar890 <ayushkumar.hp890@gmail.com>
2025-07-23 01:38:47 +0530

    give access to frontend by adding link in cors

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://vigovia-saas.vercel.app',
     credentials: true 
 }));
 
